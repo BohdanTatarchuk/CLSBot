2549fae815fc26a797d9e133e7629a155b57b527
Bohdan Tatarchuk <tatarchukbogdan80@gmail.com>
2025-01-16 23:22:59 +0100

    JDA imported. Token added. Bot can be started

--- src/main/java/org/bot/Main.java
+++ src/main/java/org/bot/Main.java
@@ -1,7 +1,12 @@
 package org.bot;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String token = ConfigUtil.getToken();
+        JDA jda = JDABuilder.createDefault(token).build();
     }
 }
